aa1a3ef378367fce77eae8a7386322ffe1dd72fe
Imre Mehesz <imehesz@gmail.com>
2022-08-03 12:27:31 -0400

    Added new sections and Util functions
    Added new sections and Util functions

--- src/Util.js
+++ src/Util.js
@@ -22,10 +22,20 @@ export const Util = {
         }
     },
 
+    StringUtil: {
+        padLeft: (str, pad, len) => (new Array(len+1).join(pad)+str).slice(-len),
+        formatTime: (time) => {
+            let minutes = Math.floor(time / 60)
+            let seconds = time - minutes * 60
+
+            return `${minutes}:${Util.StringUtil.padLeft(seconds, "0", 2)}`
+        }    
+    },
+
     ServiceUtil: {
-        loadData: async () => {
+        loadData: async (url="") => {
             try {
-                let response = await fetch(MHX.dataUrl)
+                let response = await fetch(url)
 
                 if(response.status == 200) {
                     let json = await response.json()
@@ -40,6 +50,24 @@ export const Util = {
     },
 
     StoreUtil: {
-        setMainWord: word => $mainWord = word
+        setMainWord: word => $mainWord = word,
+    },
+
+    SongUtil: {
+        /**
+         * @param raw info from stream service
+         * returns songInfoObj
+         */
+        parseSong: (rawObj) => {
+            let retObj = {}
+
+            let bandAndSongArr = rawObj.song.text.split(" - ")
+
+            retObj.bandName = bandAndSongArr[0]
+            retObj.songTitle = bandAndSongArr[1] 
+            retObj.length = Util.StringUtil.formatTime(rawObj.duration)
+
+            return retObj
+        }
     }
 }
